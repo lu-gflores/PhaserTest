f0fbbc730ccfd2fecc6a2a2bfd6319d39d693d05
George Flores <georgeflores886@gmail.com>
2020-08-04 10:49:01 -0400

    created update function for character movement

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,7 @@ const config = {
     scene: {
         preload: preload,
         create: create,
+        update: update
     },
     physics: {
         default: 'arcade', //physics engine
@@ -25,6 +26,7 @@ function preload() {
     //takes 2 arguments: key name and the location of that asset
     this.load.image('button1', 'assets/images/ui/blue_button01.png');
     this.load.spritesheet('items', 'assets/images/items.png', {frameWidth: 32 , frameHeight: 32});
+    this.load.spritesheet('characters', 'assets/images/characters.png', {frameWidth: 32 , frameHeight: 32});
 
 }
 
@@ -39,4 +41,26 @@ function create() {
     this.add.image(300, 300, 'items', 0)
     this.physics.add.image(500, 100, 'button1')
 
+    this.player = this.physics.add.image(32, 32, 'characters', 0);
+    this.player.setScale(2);
+    //detects user input on keyboard
+    this.cursors = this.input.keyboard.createCursorKeys();
+
+}
+
+function update(){
+    //
+    this.player.setVelocity(0);
+
+    if(this.cursors.left.isDown) {
+        this.player.setVelocityX(-160); 
+    } else if(this.cursors.right.isDown) {
+        this.player.setVelocityX(160); 
+    } 
+
+    if(this.cursors.up.isDown) {
+        this.player.setVelocityY(-160); 
+    } else if(this.cursors.down.isDown) {
+        this.player.setVelocityY(160); 
+    } 
 }
