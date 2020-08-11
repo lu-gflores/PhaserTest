5a99af4edd781db9c008b5484297511817d2870a
George Flores <georgeflores886@gmail.com>
2020-08-11 08:41:24 -0400

    created randomPositions method for monster movement

--- assets/js/game_manager/MonsterModel.js
+++ assets/js/game_manager/MonsterModel.js
@@ -5,14 +5,51 @@ class MonsterModel {
         this.x = x * 2;
         this.y = y * 2;
         this.gold = gold;
-        this.frame =  frame;
+        this.frame = frame;
         this.health = health;
-        this.maxHealth =  health;
+        this.maxHealth = health;
         this.attack = attack;
     }
-    
+
     loseHealth() {
         this.health -= 1;
         console.log(this.health);
+    }
+
+    move() {
+        const randomPosition = randomNumber(1, 8);
+
+        switch (randomPosition) {
+            case 1:
+                this.x += 32;
+                break;
+            case 2:
+                this.x -= 32;
+                break;
+            case 3:
+                this.y += 32;
+                break;
+            case 4:
+                this.y -= 32;
+                break;
+            case 5:
+                this.x += 32;
+                this.y += 32;
+                break;
+            case 6:
+                this.x += 32;
+                this.y -= 32;
+                break;
+            case 7:
+                this.x -= 32;
+                this.y += 32;
+                break;
+            case 8:
+                this.x -= 32;
+                this.y -= 32;
+                break;
+            default:
+                break;
+        }
     }
 }
